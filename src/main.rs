c6420a179ec53d13dcfdb7d7c0e7eb8b847f9d84
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2024-06-22 10:49:44 +0800

    Make a bootable image printing "Hello, World!"

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,25 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    // A panic handler function should never, so we use the never type `!` to indicate that
+    // A panic handler function should never
+    // so we use the never type `!` to indicate that
     loop {}
 }
 
 #[no_mangle]  // don't mangle the name of _start
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function named `_start` by default
+    // this function is the entry point
+    // since the linker looks for a function named `_start` by default
+
+    // output "Hello, World!" to the VGA buffer
+    static HELLO: &[u8] = b"Hello, World!";
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+	unsafe {
+	    *vga_buffer.offset(i as isize * 2) = byte;
+	    *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // light cyan
+	}
+    }
     loop {}
 }
 
