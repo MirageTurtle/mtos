f85d1004db4d98909bbf594f05922e4f827b459d
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2024-06-30 19:41:16 +0800

    Build a small (not complete) vga_buffer mod.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
-#![no_main]  // I need to overwrite the entry point
+#![no_main] // I need to overwrite the entry point
 
 use core::panic::PanicInfo;
+
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -10,20 +12,22 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]  // don't mangle the name of _start
+#[no_mangle] // don't mangle the name of _start
 pub extern "C" fn _start() -> ! {
     // this function is the entry point
     // since the linker looks for a function named `_start` by default
 
     // output "Hello, World!" to the VGA buffer
-    static HELLO: &[u8] = b"Hello, World!";
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-	unsafe {
-	    *vga_buffer.offset(i as isize * 2) = byte;
-	    *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // light cyan
-	}
-    }
+    // static HELLO: &[u8] = b"Hello, World!";
+    // let vga_buffer = 0xb8000 as *mut u8;
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // light cyan
+    //     }
+    // }
+
+    // use vga_buffer mod
+    vga_buffer::print_something();
     loop {}
 }
-
---
+++ src/vga_buffer.rs
@@ -0,0 +1,120 @@
+// Code for the VGA text buffer
+/**
+ * VGA text buffer
+ * 0-7: ASCII code point
+ * 8-11: foreground color
+ * 12-14: background color
+ * 15: blink
+ */
+
+// colors using an enum
+#[allow(dead_code)] // disable the warning for unused code
+#[derive(Debug, Clone, Copy, PartialEq, Eq)] // printable, copyable, comparable
+#[repr(u8)] // rust doesn't have `u4` so we use `u8` instead
+pub enum Color {
+    Black = 0,       // 0x0
+    Blue = 1,        // 0x1
+    Green = 2,       // 0x2
+    Cyan = 3,        // 0x3
+    Red = 4,         // 0x4
+    Magenta = 5,     // 0x5
+    Brown = 6,       // 0x6
+    LightGray = 7,   // 0x7
+    DarkGray = 8,    // 0x8
+    LightBlue = 9,   // 0x9
+    LightGreen = 10, // 0xA
+    LightCyan = 11,  // 0xB
+    LightRed = 12,   // 0xC
+    Pink = 13,       // 0xD
+    Yellow = 14,     // 0xE
+    White = 15,      // 0xF
+}
+
+// a full color byte
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)] // has the same memory layout as u8
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+// a screen character
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)] // the struct should have a C-like layout
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+// A writer for actually writing to the VGA buffer
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer, // the `'static` lifetime
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                // let row = BUFFER_HEIGHT - 1; // just write in the bottom line
+                let row = 0; // just write in the top line
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn new_line(&mut self) {
+        // // move all characters one row up
+        // for row in 1..BUFFER_HEIGHT {
+        //     for col in 0..BUFFER_WIDTH {
+        //         let character = self.buffer.chars[row][col];
+        //         self.buffer.chars[row - 1][col] = character;
+        //     }
+        // }
+        // self.clear_row(BUFFER_HEIGHT - 1);
+        // self.column_position = 0;
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte), // printable ASCII byte or newline
+                _ => self.write_byte(0xfe),                   // unknown character
+            }
+        }
+    }
+}
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_string("Hello, Wörld!"); // Hello, W■■rld!
+}
